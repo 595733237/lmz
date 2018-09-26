4b5ac37e39abfa8f02b43034d4d72029ae02b2bc
tuziel <tuziel@outlook.com>
2018-09-26 15:49:05 +0800

    update: 添加element-ui辅助样式

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
- 
+
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
- 
+import './styles/theme.css'
+
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap.min.css'  
-import 'bootstrap/dist/js/bootstrap.min.js' 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 
 Vue.config.productionTip = false
