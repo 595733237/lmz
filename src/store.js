262aaf13c79d8076e7ea32399a7d7afe6a31f087
tuziel <tuziel@outlook.com>
2018-09-30 14:13:30 +0800

    update: 使用store管理问卷数据

--- src/store.js
+++ src/store.js
@@ -5,12 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    // test: '123'
+    questionnaires: [
+      // {
+      //   title: '问卷标题',
+      //   // 1: 单选, 2: 多选, 3: 文本
+      //   type: 1,
+      //   questions: [{
+      //     title: '问题',
+      //     answers: []
+      //   }]
+      // }
+    ]
   },
   mutations: {
-    // setTest(state, data) {
-    //   state.test = data
-    // }
+    // 增加问卷
+    createQuest(state, quest) {
+      state.questionnaires.push(quest)
+    },
+    // 删除问卷
+    removeQuest(state, quest) {
+      // 找到当前下标并移除
+      const questionnaires = state.questionnaires
+      const index = questionnaires.indexOf(quest)
+      questionnaires.splice(quest, 1)
+    }
   },
   actions: {
 
